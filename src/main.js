b00f860ea112ed8da54ef9bf3808ab57c15a8d7c
Karina <karina.oyarzunh@gmail.com>
2021-10-07 12:59:31 -0300

    Creación de maqueta

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+// Función para aparecer y desaparecer páginas
+const firstPage = document.getElementById("firstPage");
+const secondPage = document.getElementById("secondPage");
+
+document.getElementById("buttonEnter").addEventListener("click", () => {
+    firstPage.style.display = "none";
+    secondPage.style.display = "block";
+});
