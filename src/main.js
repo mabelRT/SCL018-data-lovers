6f4b7537c30739f53603cdf3c3f34e79daa02f64
mabel <wendy.torrico.wt@gmail.com>
2021-10-25 23:05:03 -0300

    boton de eliminar terminado

--- src/main.js
+++ src/main.js
@@ -100,35 +100,43 @@ filterStatus.addEventListener("change", (event) => {
     filter(status);
 });
 
-//boton limpiar funcional(no limpia los selct)
-/* const buttonCleanReset = document.getElementById("buttonClean");
-buttonCleanReset.addEventListener("click", () => {
-    printCharacters.innerHTML = "";
-    function clean() {
-        for (let i = 0; i < data.results.length; i++) {
-            printCharacters.innerHTML += drawCard(data.results[i]);
-        }
-    }
-    clean(data.results);
-});
-buttonCleanReset.addEventListener("click", () =>{
-    
-}) */
-
-//boton limpiar funcional(no limpia los selct)
+/*boton limpiar funcional(no limpia los selct)
 const buttonCleanReset = document.getElementById("buttonClean");
-
 buttonCleanReset.addEventListener("click", () => {
     printCharacters.innerHTML = "";
     function clean() {
-
         for (let i = 0; i < data.results.length; i++) {
             printCharacters.innerHTML += drawCard(data.results[i]);
         }
     }
     clean(data.results);
+});*/
 
-});
-buttonCleanReset.addEventListener("click", () =>{
+
+ 
+
+
+const buttonCleanReset = document.getElementById("bottonClean");
+
+/*buttonCleanReset.addEventListener('click',cleanCard);*/
+buttonCleanReset.addEventListener('click',resetElement);
+
+ function resetElement(){
+    cleanStatus(); 
+    cleanCard();
     
-})
+ }
+function cleanCard() {
+    printCharacters.innerHTML = "";
+    for (let i = 0; i < rickandmorty.length; i++) {
+        printCharacters.innerHTML += drawCard(rickandmorty[i]);
+    }
+
+}
+
+function cleanStatus() {
+    document.getElementById("filterStatus").selectedIndex = 0;
+    document.getElementById("filterSpecies").selectedIndex = 0; 
+    document.getElementById("sort").selectedIndex = 0;        
+
+}
